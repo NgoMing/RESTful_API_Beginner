1f5ebdcdafb0b69bc096e136990ac493b5391cd7
minhnln <mingngo158@gmail.com>
2018-05-04 01:48:29 +1000

    Configureing for using Spring MVC

---
+++ src/main/java/onlineshop/config/RootConfig.java
@@ -0,0 +1,9 @@
+package onlineshop.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "onlineshop" })
+public class RootConfig {
+}
---
+++ src/main/java/onlineshop/config/WebConfig.java
@@ -0,0 +1,34 @@
+package onlineshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("onlineshop")
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+    /**
+     * Configuration for JSP view format
+     */
+    @Bean
+    public ViewResolver viewResolver() {
+
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setExposeContextBeansAsAttributes(true);
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling( DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
